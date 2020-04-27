288412754bfa20c548a26bafd0aa9ca6ccc3cdd1
i300981 <herbert.zhao@sap.com>
2020-04-27 10:08:57 +0800

    initialize project

---
+++ index.js
@@ -0,0 +1,1 @@
+export { default as AdvancedList } from "./src/index";
---
+++ src/index.js
@@ -0,0 +1,192 @@
+import React, { Component } from 'react'
+import {
+  ScrollView,
+  View,
+  TouchableHighlight,
+} from 'react-native'
+
+import PropTypes from 'prop-types'
+
+export default class AdvancedList extends Component {
+
+  static propTypes = {
+    leftList: PropTypes.shape({
+      listHeader: PropTypes.func,
+      sectionHeader: PropTypes.func,
+      renderItem: PropTypes.func.isRequired,
+      sections: PropTypes.array.isRequired,
+    }).isRequired,
+    rightList: PropTypes.shape({
+      listHeader: PropTypes.func,
+      sectionHeader: PropTypes.func,
+      renderItem: PropTypes.func.isRequired,
+      sections: PropTypes.array.isRequired,
+      contentContainerWidth: PropTypes.number.isRequired,
+    }).isRequired,
+  }
+
+  // static defaultProps = {
+  //   leftList: {
+  //     listHeader: () => <View />,
+  //     sectionHeader: (section, sectionIndex) => <View />,
+  //   },
+  //   rightList: {
+  //     listHeader: () => <View />,
+  //     sectionHeader: (section, sectionIndex) => <View />,
+  //   }
+  // }
+
+  constructor(props) {
+    super(props)
+
+    this.rightScrollViewOffsetX = 0
+    this.rightScrollViewOffsetY = 0
+    this.frozenScrollViewBeginDragging = false
+    this.rightHeaderScrollViewBeginDragging = false
+
+    this.state = {
+      leftScrollView: null,
+      rightScrollView: null,
+      rightHeaderScrollView: null,
+
+      leftStickyHeaderIndices: [],
+      rightStickyHeaderIndices: [],
+
+      leftSections: [],
+      rightSections: [],
+
+      leftListHeader: () => <View />,
+      leftSectionHeader: (section, sectionIndex) => <View />,
+      rightListHeader: () => <View />,
+      rightSectionHeader: (section, sectionIndex) => <View />,
+    }
+  }
+
+  componentDidMount() {
+
+    if (this.props.leftList.listHeader !== undefined) {
+      this.setState({ leftListHeader: this.props.leftList.listHeader })
+    }
+    if (this.props.leftList.sectionHeader !== undefined) {
+      this.setState({ leftSectionHeader: this.props.leftList.sectionHeader })
+    }
+    if (this.props.rightList.listHeader !== undefined) {
+      this.setState({ rightListHeader: this.props.rightList.listHeader })
+    }
+    if (this.props.rightList.sectionHeader !== undefined) {
+      this.setState({ rightSectionHeader: this.props.rightList.sectionHeader })
+    }
+
+    this.setState({ leftSections: this.props.leftList.sections })
+    this.setState({ rightSections: this.props.rightList.sections })
+
+    this.setState({ leftStickyHeaderIndices: this._createStickyHeaderIndices(this.props.leftList.sections) })
+    this.setState({ rightStickyHeaderIndices: this._createStickyHeaderIndices(this.props.rightList.sections) })
+
+  }
+
+  _createStickyHeaderIndices(sections) {
+    var indices = []
+    for (idx = 0; idx < sections.length; idx++) {
+      idx === 0 ? indices.push(idx) : indices.push(indices[idx - 1] + sections[idx - 1].data.length + 1)
+    }
+    return indices
+
+  }
+
+  render() {
+
+    return (
+      <View
+        key={'favView'}
+        style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start' }}>
+        <View>
+          {this.state.leftListHeader()}
+          <ScrollView
+            scrollEventThrottle={16}
+            key={'leftscrollview'}
+            ref={(ref) => this.state.leftScrollView = ref}
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+            stickyHeaderIndices={this.state.leftStickyHeaderIndices}
+            onScrollBeginDrag={() => this.frozenScrollViewBeginDragging = true}
+            onScroll={(event) => {
+              if (this.frozenScrollViewBeginDragging) {
+                this.state.rightScrollView.scrollTo({ x: this.rightScrollViewOffsetX, y: event.nativeEvent.contentOffset.y, animated: false });
+              }
+            }}
+          >
+            {this.state.leftSections.map((section, sectionIndex) => {
+              var header = <TouchableHighlight key={sectionIndex}>
+                {this.state.leftSectionHeader(section, sectionIndex)}
+              </TouchableHighlight>
+              var items = section.data.map((item, itemIndex) =>
+                this.props.leftList.renderItem(section, sectionIndex, item, itemIndex)
+              )
+              // console.log("tempcomponents " + JSON.stringify([header, items]))
+              return [header, items]
+            }
+            )}
+          </ScrollView>
+        </View>
+        <View style={{ flex: 1 }}>
+          <View>
+            <ScrollView
+              horizontal={true}
+              scrollEventThrottle={16}
+              showsHorizontalScrollIndicator={false}
+              showsVerticalScrollIndicator={false}
+              ref={(ref) => this.state.rightHeaderScrollView = ref}
+              onScrollBeginDrag={() => this.rightHeaderScrollViewBeginDragging = true}
+              onScroll={(event) => {
+                if (this.rightHeaderScrollViewBeginDragging) {
+                  this.state.rightScrollView.scrollTo({ x: event.nativeEvent.contentOffset.x, y: this.rightScrollViewOffsetY, animated: false });
+                }
+              }}
+            >
+              {
+                this.state.rightListHeader()
+              }
+            </ScrollView>
+          </View>
+
+          <ScrollView
+            directionalLockEnabled={true}
+            scrollEventThrottle={16}
+            key={'rightscrollview'}
+            ref={(ref) => this.state.rightScrollView = ref}
+            showsHorizontalScrollIndicator={false}
+            stickyHeaderIndices={this.state.rightStickyHeaderIndices}
+            onScrollBeginDrag={() => {
+              this.frozenScrollViewBeginDragging = false
+              this.rightHeaderScrollViewBeginDragging = false
+            }}
+            onScroll={(event) => {
+              if (!this.frozenScrollViewBeginDragging) {
+                this.state.leftScrollView.scrollTo({ x: 0, y: event.nativeEvent.contentOffset.y, animated: false });
+              }
+              if (!this.rightHeaderScrollViewBeginDragging) {
+                this.state.rightHeaderScrollView.scrollTo({ x: event.nativeEvent.contentOffset.x, y: 0, animated: false })
+              }
+              this.rightScrollViewOffsetX = event.nativeEvent.contentOffset.x
+              this.rightScrollViewOffsetY = event.nativeEvent.contentOffset.y
+            }}
+            contentContainerStyle={{ width: this.props.rightList.contentContainerWidth }}>
+            {
+              this.state.rightSections.map((section, sectionIndex) => {
+                var header = <TouchableHighlight key={sectionIndex}>
+                  {this.state.rightSectionHeader(section, sectionIndex)}
+                </TouchableHighlight>
+                var items = section.data.map((item, itemIndex) =>
+                  this.props.rightList.renderItem(section, sectionIndex, item, itemIndex)
+                )
+                return [header, items]
+              }
+              )
+            }
+          </ScrollView>
+        </View>
+      </View>
+    )
+  }
+}
