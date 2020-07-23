72152e5325b618e8d21fcd10bdb0d91fdd0b826e
Lucia <lucia.wu02@sap.com>
2020-07-23 10:42:45 +0800

    AdvancedColorList

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as AdvancedList } from "./src/index";
 export { default as AdvancedFullScreenList } from "./src/FullScreenList";
+export { default as AdvancedColorList } from "./src/AdvancedColorList";
